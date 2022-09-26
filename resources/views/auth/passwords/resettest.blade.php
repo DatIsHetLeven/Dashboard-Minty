7ee24ccf7d49218921a77dfe037fc838f887415b
DatIsHetLeven <muratcelem187@gmail.com>
2022-09-26 11:23:31 +0200

    Home pagina
    Terug naar de home pagina via de banner 'MINTY MEDIA'

--- resources/views/auth/passwords/resettest.blade.php
+++ resources/views/auth/passwords/resettest.blade.php
@@ -2,7 +2,7 @@
 <link href="{{ asset('css/logintest.css') }}" rel="stylesheet"> 
 <meta name="viewport" content="width=device-width, initial-scale=0.8">
 <br><br></br></br>
-<h3><span id="minty"> MINTY</span> MEDIA<h3>
+<h3><a href="{{ route('login') }}" span id="minty" > MINTY</span><span id="media"> MEDIA</span></a><h3>
 <h5><span id="dashboard">DASHBOARD</span></h5>
 
 <div class="formlogin">
